9da6060c9cd581b4a81816c91be516d9a8fe4ced
jsacdalan <jsacdalan@ltu.edu>
2018-02-27 17:54:44 -0500

    A ton of stuff
    So close. I have frankensteined some stack overflow posts together to somehow come up wiht this code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 
@@ -12,9 +18,9 @@ public class Main {
 private static int lineCount;
 
 
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException {
 	String fileName;
-	System.out.println("Input File Path: ");
+	System.out.println("Input File Name: ");
 	
 	Scanner userInput = new Scanner(System.in);
 	fileName = userInput.nextLine();
@@ -23,13 +29,15 @@ public static void main(String[] args) {
 	
 	
 	fileName = new File(fileName).getName();
-	System.out.println(fileName);
+	System.out.println("File Name: " + fileName);
 	System.out.println("LOC: " + lineCount);
+	userInput.close();
 }
 
-static int countLOC(String fileName) {
+static int countLOC(String fileName) throws IOException {
+	String line = "";
 	Scanner scan = null;
-	
+	//Setup for counting the number of blank spaces
 	try {
 		File file = new File(fileName);
 		scan = new Scanner(file);
@@ -37,12 +45,58 @@ static int countLOC(String fileName) {
 		e.printStackTrace();
 	}
 	while(scan.hasNext()) {
-		String line =  (String) scan.nextLine();
+		line =  (String) scan.nextLine();
 		if(line != null) {
 			lineCount++;
 		}
+		if(line.trim().isEmpty()) { //Subtract number of blank lines from lineCount value
+			lineCount--;
+		}
 	}
-	return lineCount;
+
+//	InputStream is = new BufferedInputStream(new FileInputStream(fileName));
+//    try {
+//    	//Setup for counting number of lines of code not including comments
+//        byte[] c = new byte[1024];
+//        int count = 0;
+//        int readChars = 0;
+//        boolean empty = true;
+//        while ((readChars = is.read(c)) != -1)
+//        {
+//            empty = false;
+//            for (int i = 0; i < readChars; ++i)
+//            {
+//                if (c[i] == '\n')
+//                    ++lineCount;
+//            }
+//        }
+//        return (count == 0 && !empty) ? 1 : count;
+//    }
+//    finally
+//    {
+//        is.close();
+//    }
+
+    	BufferedReader fileRead = new BufferedReader(new FileReader(fileName));
+    	while ((line = fileRead.readLine()) != null) {
+            if (line.contains("//")) {
+                lineCount--;
+            } 
+            else if (line.contains("**")) {
+            	lineCount--;
+            }
+            else if (line.contains("/*")) {
+                lineCount--;
+                while (!line.contains("*/") && !(line = fileRead.readLine()).contains("*/"));
+            }
+        }
+        fileRead.close();
+        
+        return lineCount;
+    } 
+    
+    
 }
 
-}
+
+
