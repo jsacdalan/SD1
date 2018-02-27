b002f645e69e9db33e689b46691cd1688cd19f79
jsacdalan <jsacdalan@ltu.edu>
2018-02-26 19:50:48 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+private static int lineCount;
+
+
+
+public static void main(String[] args) {
+	String filePath;
+	System.out.println("Input File Path: ");
+	
+	Scanner userInput = new Scanner(System.in);
+	filePath = userInput.nextLine();
+	
+	Scanner scan = null;
+	try {
+		File file = new File(filePath);
+		scan = new Scanner(file);
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	while(scan.hasNext()) {
+		String line =  (String) scan.nextLine();
+		if(line != null) {
+			lineCount++;
+		}
+	}
+	
+	String fileName = new File(filePath).getName();
+	System.out.println(fileName);
+	System.out.println("LOC: " + lineCount);
+}
+
+
+}
